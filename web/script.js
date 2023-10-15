f5ada5bb71698c95b621b40384b6f94c9269cf8c
yaxf <sayyafmuhammad22@gmail.com>
2023-10-15 20:41:09 +0700

    first commit

---
+++ web/script.js
@@ -0,0 +1,18 @@
+// toggle class active
+const navbarNav = document.querySelector ('.navbar-nav');
+
+// ketika hamburger menu diklik
+document.querySelector('#hamburger-menu').onclick = () => 
+{
+    navbarNav.classList.toggle('active');
+};
+
+// ketika diklik di luar sidebar untuk menghilangkan nav
+const hamburger = document.querySelector('#hamburger-menu');
+
+document.addEventListener('click', function(e) {
+    if(!hamburger.contains(e.target) && !navbarNav.contains(e.target))
+    {
+        navbarNav.classList.remove('active');
+    }
+});
